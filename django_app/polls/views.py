b29b69c16b80fe095dd50a651f7c4e7fc86f33fb
Hernán D. Mercado <hernandmf@gmail.com>
2022-09-05 07:36:39 -0500

    crating the views

--- django_app/polls/views.py
+++ django_app/polls/views.py
@@ -4,4 +4,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello world")
+    return HttpResponse("Estás en la página principal de premios platiz app")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estás viend la pregunta número {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunto número {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estás votando a la pregunta número {question_id}")
