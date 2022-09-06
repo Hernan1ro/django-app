346c0bc17565bbf7a5ca5068fabda18029d5bb3b
Hernán D. Mercado <hernandmf@gmail.com>
2022-09-06 18:10:35 -0500

    creating the views for result and detail

--- django_app/polls/views.py
+++ django_app/polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.all()
@@ -15,8 +16,23 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f"Estas viendo los resultados de la pregunto número {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse(f"Estás votando a la pregunta número {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist): 
+        return render(request, "polls/detail.html", {
+            'question': question, 
+            "error_message": "No elegiste una respuesta"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+
+
